415940da084ff61526d7605f30bedb8b82758f5c
nathenbyford <nathenbyford@gmial.com>
2020-11-14 17:47:04 -0600

    EDA

--- Scripts/eda.r
+++ Scripts/eda.r
@@ -1,1 +1,20 @@
 library(tidyverse)
+library(car)
+library(ggally)
+library(olsrr)
+
+data <- read_csv("~/Downloads/globalterrorismdb_0718dist.csv")
+
+datus <- data %>% filter(country == 217)
+
+pred <- datus %>% select(imonth, provstate, suicide, attacktype1, targtype1, 
+                         weaptype1, propextent)
+
+res <- datus %>% select(success)
+
+## Split the data 80% 20%
+
+
+
+
+## make a full model
