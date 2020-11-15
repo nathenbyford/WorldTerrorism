1c7c43c5f2b5c752668261be5c697529bc95864b
nathenbyford <nathenbyford@gmial.com>
2020-11-14 18:03:25 -0600

    Full model

--- Scripts/eda.r
+++ Scripts/eda.r
@@ -2,6 +2,8 @@ library(tidyverse)
 library(car)
 library(ggally)
 library(olsrr)
+library(carets)
+library(leaps)
 
 data <- read_csv("~/Downloads/globalterrorismdb_0718dist.csv")
 
@@ -12,9 +14,29 @@ pred <- datus %>% select(imonth, provstate, suicide, attacktype1, targtype1,
 
 res <- datus %>% select(success)
 
+datus_s <- cbind(res, pred)
+
 ## Split the data 80% 20%
 
+set.seed(2)
+sample <- sample.int(n = nrow(datus_s), size = floor(.80*nrow(datus_s)), 
+                     replace = FALSE)
 
+train <- datus_s[sample, ]
+test <- datus_s[-sample, ]
 
 
 ## make a full model
+
+full <- glm(success ~ imonth+provstate+suicide+factor(attacktype1)+factor(targtype1)+
+              factor(weaptype1)+factor(propextent), data = datus_s)
+
+summary(full)
+
+par(mfrow = c(2, 2))
+plot(full)
+
+par(mfrow = c(1, 1))
+
+## Use lasso for model selection
+
